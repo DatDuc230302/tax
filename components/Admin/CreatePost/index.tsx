f2e8c8a08a5839c6a7f24ef7b974278fbce86b64
haotran0103 <83738152+haotran0103@users.noreply.github.com>
2023-10-26 10:25:26 +0700

    jodit editor

--- components/Admin/CreatePost/index.tsx
+++ components/Admin/CreatePost/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, useRef, useMemo } from 'react';
 
 import {
     Modal,
@@ -24,8 +24,7 @@ import { isDate } from '@/functions/isDate';
 import axios from 'axios';
 import { AdminContext } from '@/app/admin/layout';
 import SnackbarMessage from '@/components/Common/SnackbarMessage';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import JoditEditor from 'jodit-react';
 export default function CreatePost({
     refresh,
     setRefresh,
@@ -128,60 +127,18 @@ export default function CreatePost({
             setImage(null);
         }
     };
+    const editor = useRef(null);
+        const [editorContent, setEditorContent] = useState('');
+        const placeholder = 'Start typing...';
+        const config = useMemo(() => ({
+            readonly: false,
+            placeholder: placeholder || 'Start typing...',
+            uploader: {
+                insertImageAsBase64URI: false, 
+                url: `${serverBackend}/api/v1/upload-images`, 
+            },
+        }), [placeholder]);
 
-    const editorConfiguration ={
-        toolbar: {
-			items: [
-				'ckbox',
-				'|',
-				'codeBlock',
-				'|',
-				'exportPdf',
-				'exportWord',
-				'|',
-				'fontBackgroundColor',
-				'fontSize',
-				'highlight',
-				'style',
-				'-',
-				'heading',
-				'bold',
-				'italic',
-				'link',
-				'bulletedList',
-				'numberedList',
-				'outdent',
-				'indent',
-				'alignment',
-				'imageUpload',
-				'blockQuote',
-				'insertTable',
-				'mediaEmbed',
-				'undo',
-				'fontFamily',
-				'fontColor',
-				'redo'
-			],
-    },
-    language: 'vi',
-		image: {
-			toolbar: [
-				'imageTextAlternative',
-				'toggleImageCaption',
-				'imageStyle:inline',
-				'imageStyle:block',
-				'imageStyle:side'
-			]
-		},
-		table: {
-			contentToolbar: [
-				'tableColumn',
-				'tableRow',
-				'mergeTableCells'
-			]
-		}
-		
-	};
     return (
         <>
             {status === 'success' && <SnackbarMessage type={1} title="Thêm bài đăng thành công" />}
@@ -296,44 +253,13 @@ export default function CreatePost({
                             </div>
                         </div>
                         {/* <TextEditor/> */}
-                         <CKEditor
-                    editor={ ClassicEditor }
-                    data="<p>Hello from CKEditor 5!</p>"
-                    config={{
-                        ckfinder: {
-                            // Upload the images to the server using the CKFinder QuickUpload command.
-                            uploadUrl: '/ckeditor5-ckfinder3-react-php/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files&responseType=json',
-                            // Define the CKFinder configuration (if necessary).
-                            options: {
-                                resourceType: 'Images'
-                            }
-                        },
-               
-                      toolbar: [ 'Essentials', 'CKFinderUploadAdapter', 'Autoformat', 'Bold', 'Italic', 'BlockQuote', 'CKBox', 'CKFinder', 'CloudServices', 'EasyImage', 'Heading', 'Image', 'ImageCaption', 'ImageStyle', 'ImageToolbar', 'ImageUpload', 'Indent', 'Link', 'List', 'MediaEmbed', 'Paragraph', 'PasteFromOffice', 'PictureEditing', 'Table', 'TableToolbar', 'TextTransformation' ],
-                      heading: {
-                          options: [
-                              { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                              { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                              { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-                          ]
-                      }
-                    }}
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event: any, editor: any ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
-
+                         <JoditEditor
+                        ref={editor}
+                        value={content}
+                        config={config}
+                        onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+                        onChange={newContent => {}}
+                    />
                         <input onChange={(e) => handleUploadImg(e)} id="uploadImg" type="file" hidden />
                     </ModalBody>
                     <ModalFooter>
---
+++ setting/index.ts
@@ -0,0 +1,8 @@
+export const theme_color: string = '#e9afaf';
+export const footer_color : string = '#e9afaf';
+export const footer_owner : string = 'Chi cục thuế Quận 8';
+export const footer_address : string = '1079 Phạm Thế Hiển, Phường 5, Quận 8, Thành phố Hồ Chí Minh';
+export const footer_phone : string = '(028) 3850 6081';
+export const footer_email  : string = 'Hotrokhoinghiep_q8.hcm@gdt.gov.vn';
+export const footer_working_hours : string = '07:30–11:30, 13:00–16:30 từ t2-t6';
+export const footer_website : string = 'chicucthuequan8.com';
