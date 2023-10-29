ccfcf3a48d4c95cc4b3f6c04497501b11293604d
Dat Duc <DatDuc2303@gmail.com>
2023-10-29 09:37:18 +0700

    Fix Slider

--- components/Client/Slide/index.tsx
+++ components/Client/Slide/index.tsx
@@ -1,37 +1,33 @@
 'use client';
 
 import React from 'react';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Slider from 'react-slick';
 import Image from 'next/image';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function Slide() {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    };
-
     const slides = ['/imgs/slides/slide1.jpg', '/imgs/slides/slide2.jpg', '/imgs/slides/slide3.jpg'];
 
     return (
-        <div className="flex justify-center">
+        <div className="flex justify-center px-4">
             <div className="flex w-wMain justify-center">
-                <Slider
-                    className="flex md1:w-full md:w-[700px] w-[280px] xs:w-[420px] flex-col items-center"
-                    {...settings}
+                <Carousel
+                    autoPlay={true}
+                    infiniteLoop={true}
+                    transitionTime={500}
+                    interval={5000}
+                    showArrows={false}
+                    showStatus={false}
+                    className="w-full"
+                    emulateTouch={true}
+                    swipeable={true}
                 >
-                    {slides.map((item: string, index: number) => (
-                        <div key={index} className="flex relative md1:w-full md1:h-[600px] md:h-[500px] h-[300px]">
-                            <Image className="object-cover" src={item} sizes="100000px" fill alt="" />
+                    {slides.map((item: any) => (
+                        <div className="w-full h-[800px]" key={item}>
+                            <Image src={item} className="object-cover" fill sizes="100000px" alt="Slide 1" />
                         </div>
                     ))}
-                </Slider>
+                </Carousel>
             </div>
         </div>
     );
