4346fa799b07f3c0c90f56170aef87bd228ff89c
haotran0103 <83738152+haotran0103@users.noreply.github.com>
2023-10-29 12:03:51 +0700

    slide

--- components/Admin/SlidesAdmin/index.tsx
+++ components/Admin/SlidesAdmin/index.tsx
@@ -5,19 +5,19 @@ import { Button, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow
 import Image from 'next/image';
 import { BsFillTrashFill, BsPlusCircle } from 'react-icons/bs';
 import { MdRestore } from 'react-icons/md';
+import axios from 'axios';
+import { serverBackend } from '@/server';
 
 export default function BannersAdmin() {
     const [banners, setBanners] = useState([]);
     const [imageUrl, setImageUrl] = useState('');
     const [status, setStatus] = useState('active');
+    const [imageFile, setImageFile] = useState(null);
 
-
-    // Function to fetch banner images from the API
-    const fetchBannerImages = async () => {
+     const fetchBannerImages = async () => {
         try {
-            const response = await fetch('/api/v1/banner-images'); 
-            const data = await response.json();
-            setImageUrl(data);
+            const response = await axios.get(`${serverBackend}/api/v1/banner-images`);
+            setBanners(response.data);
         } catch (error) {
             console.error('Error fetching banner images:', error);
         }
@@ -27,34 +27,61 @@ export default function BannersAdmin() {
         fetchBannerImages();
     }, []);
 
+    const handleAddBanner = async () => {
+        try {
+            const newBanner = {
+                image_url: imageUrl,
+            };
 
-    const handleAddBanner = () => {
-        // Gửi yêu cầu POST để thêm banner vào API
-        // Thực hiện gửi yêu cầu POST vào đây
-        // Sau khi thêm thành công, cập nhật danh sách banner
+            await axios.post(serverBackend, newBanner);
+            fetchBannerImages(); // Sau khi thêm thành công, cập nhật danh sách banner
+        } catch (error) {
+            console.error('Error adding banner:', error);
+        }
     };
 
-    const handleDeleteBanner = (id) => {
-        // Gửi yêu cầu DELETE để xóa banner với ID cụ thể
-        // Thực hiện gửi yêu cầu DELETE vào đây
-        // Sau khi xóa thành công, cập nhật danh sách banner
+    const handleDeleteBanner = async (id:any) => {
+        try {
+            await axios.delete(`${serverBackend}/api/v1/banner-images/${id}`);
+            fetchBannerImages(); // Sau khi xóa thành công, cập nhật danh sách banner
+        } catch (error) {
+            console.error('Error deleting banner:', error);
+        }
     };
 
-    const handleUpdateBanner = (id) => {
-        // Gửi yêu cầu PUT để cập nhật thông tin banner với ID cụ thể
-        // Thực hiện gửi yêu cầu PUT vào đây
-        // Sau khi cập nhật thành công, cập nhật danh sách banner
+    const handleUpdateBanner = async (id:any) => {
+        try {
+            const updatedBanner = {
+                image_url: imageUrl,
+                status: status,
+            };
+
+            await axios.put(`${serverBackend}/api/v1/banner-images/${id}`, updatedBanner);
+            fetchBannerImages(); // Sau khi cập nhật thành công, cập nhật danh sách banner
+        } catch (error) {
+            console.error('Error updating banner:', error);
+        }
+    };
+    const handleImageUpload = (e: any) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                setImageFile(file);
+                setImageUrl(event.target.result);
+            };
+            reader.readAsDataURL(file);
+        }
     };
     return (
-        <div className="flex w-full px-4 mt-4 justify-center">
+         <div className="flex w-full px-4 mt-4 justify-center">
             <div className="flex w-full flex-col gap-3">
                 <div className="flex justify-end">
                     <div className="flex gap-2">
                         <input
-                            type="text"
-                            placeholder="Image URL"
-                            value={imageUrl}
-                            onChange={(e) => setImageUrl(e.target.value)}
+                            type="file"
+                            accept="image/*"
+                            onChange={handleImageUpload}
                         />
                         <select value={status} onChange={(e) => setStatus(e.target.value)}>
                             <option value="active">Active</option>
