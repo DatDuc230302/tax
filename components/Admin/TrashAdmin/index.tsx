0f1bb3edea1a23935714b4a1c11394510f0c3b51
Dat Duc <DatDuc2303@gmail.com>
2023-10-31 12:44:44 +0700

    add trashAdmin

--- components/Admin/TrashAdmin/index.tsx
+++ components/Admin/TrashAdmin/index.tsx
@@ -2,16 +2,18 @@
 
 import { Tab, Tabs } from '@nextui-org/react';
 import React from 'react';
+import TrashAccounts from '../TrashAccounts';
+import TrashPosts from '../TrashPosts';
 
 export default function TrashAdmin() {
     return (
-        <div className="flex flex-col p-4">
+        <div className="flex w-full flex-col p-4">
             <Tabs aria-label="Dynamic tabs">
                 <Tab key={'Accounts'} title={'Tài khoản'}>
-                    Tài khoản
+                    <TrashAccounts />
                 </Tab>
                 <Tab key={'Posts'} title={'Bài đăng'}>
-                    Bài đăng
+                    <TrashPosts />
                 </Tab>
             </Tabs>
         </div>
