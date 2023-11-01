5273485f66f641b54b3444bcef56f2c8951460a5
haotran0103 <83738152+haotran0103@users.noreply.github.com>
2023-11-01 20:56:39 +0700

    footer

--- components/Client/Footer/index.tsx
+++ components/Client/Footer/index.tsx
@@ -1,38 +1,63 @@
+ "use client";
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { serverBackend } from '@/server';
+
 
 export default function Footer() {
+    const [settingData, setSettingData] = useState<any>(null);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem('settingData');
+    if (storedData) {
+      setSettingData(JSON.parse(storedData));
+    } else {
+      axios.get(`${serverBackend}/api/v1/ReadSetting`) 
+        .then((response) => {
+          setSettingData(response.data);
+          localStorage.setItem('settingData', JSON.stringify(response.data));
+        })
+        .catch((error) => {
+          console.error('Lỗi khi gọi API:', error);
+        });
+    }
+  }, []);
     return (
-        <div className="w-full flex justify-center bg-[#0B80FF] p-4">
-            <div className="flex w-wMain flex-col lg:flex-row justify-between text-white">
-                <div className="flex h-[60px] gap-2 items-center">
-                    <div className="w-[60px] h-[60px] relative">
-                        <Image src={'/imgs/logo.png'} fill sizes="100000px" alt="" />
-                    </div>
-                    <div className="flex flex-col">
-                        <h3 className="font-bold text-[18px]">CỤC THUẾ TP. HỒ CHÍ MINH</h3>
-                        <h3 className="font-bold text-[18px]">CHI CỤC THUẾ QUẬN 8</h3>
-                    </div>
-                </div>
-                <div className="flex gap-2 flex-col">
-                    <h2 className="font-bold text-[18px] h-[60px] items-center flex">
-                        Trang thông tin Chi Cục Thuế Quận 8
-                    </h2>
-                    <div className="flex flex-col text-[14px]">
-                        <span>Địa chỉ: Số 1079 Phạm Thế Hiển, Phường 5, Quận 8, TP Hồ Chí Minh</span>
-                        <span>Điện thoại: (028) 38 501 032. Fax: (028) 38 501 032</span>
-                        <span>Thời gian làm việc: Từ thứ 2 đến thứ 6, thứ 7 làm việc buổi sáng</span>
-                        <span>- Sáng: từ 07:30 đến 11:30</span>
-                        <span>- Chiều: từ 13g00 đến 17g00</span>
-                    </div>
-                </div>
-                <div className="flex gap-2 flex-col">
-                    <h2 className="font-bold text-[18px] h-[60px] items-center flex">Kết nối Chi Cục Thuế Quận 8</h2>
-                    <div className="flex flex-col text-[14px]">
-                        <span>© Bản quyền 2022 - Chi Cục Thuế Quận 8</span>
-                    </div>
-                </div>
+         <div className="w-full flex justify-center bg-[#0B80FF] p-4">
+
+      {settingData ? (
+        <div className="flex w-wMain flex-col lg:flex-row justify-between text-white">
+          <div className="flex h-[60px] gap-2 items-center">
+            <div className="w-[60px] h-[60px] relative">
+              <Image src={'/imgs/logo.png'} fill sizes="100000px" alt="" />
             </div>
+            <div className="flex flex-col">
+                <h3 className="font-bold text-[18px]">CỤC THUẾ THÀNH PHỐ HỒ CHÍ MINH</h3>
+              <h3 className="font-bold text-[18px]">{settingData.footer_owner}</h3>
+              
+            </div>
+          </div>
+          <div className="flex gap-2 flex-col">
+            <h2 className="font-bold text-[18px] h-[60px] items-center flex">
+              Trang thông tin Chi Cục Thuế Quận 8
+            </h2>
+            <div className="flex flex-col text-[14px]">
+              <span>Địa chỉ: {settingData.footer_address}</span>
+              <span>Điện thoại: {settingData.footer_phone}. Fax: (028) 38 501 032</span>
+              <span>Thời gian làm việc: {settingData.footer_working_hours}</span>
+            </div>
+          </div>
+          <div className="flex gap-2 flex-col">
+            <h2 className="font-bold text-[18px] h-[60px] items-center flex">Kết nối Chi Cục Thuế Quận 8</h2>
+            <div className="flex flex-col text-[14px]">
+              <span>© Bản quyền 2022 - Chi Cục Thuế Quận 8</span>
+            </div>
+          </div>
         </div>
+      ) : (
+        <div>Đang tải...</div>
+      )}
+    </div>
     );
 }
