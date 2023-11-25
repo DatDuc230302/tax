b235f3208a020412a89e5232397e815b83e5080e
haotran0103 <83738152+haotran0103@users.noreply.github.com>
2023-11-25 19:34:10 +0700

    docker

--- next.config.js
+++ next.config.js
@@ -2,8 +2,17 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ['media.hcmtax.gov.vn', 'media.npr.org','127.0.0.1','https://57f2-2a09-bac1-7a80-8-00-1f1-1c8.ngrok-free.app'],
+        domains: ['media.hcmtax.gov.vn', 'media.npr.org','127.0.0.1:8080'],
+        remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8080',
+        pathname: '/uploads/**',
+      },
+    ],
     },
+    output: 'standalone'
 };
 
 module.exports = nextConfig;
