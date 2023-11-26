3f242b705b48e17accc25779da05248e27af3aac
haotran0103 <83738152+haotran0103@users.noreply.github.com>
2023-11-26 23:56:51 +0700

    deploy

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,3 @@
 export const serverImages: string = 'http://localhost:5000';
 // export const serverBackend: string = 'http://localhost:5000';
-export const serverBackend: string = 'http://0.tcp.ap.ngrok.io:16968';
+export const serverBackend: string = 'http://128.199.226.117:9999';
