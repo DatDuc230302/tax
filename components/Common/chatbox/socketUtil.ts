df9fea3dbfb471603fe072dc0ae5296440c0cf4e
haotran0103 <83738152+haotran0103@users.noreply.github.com>
2023-12-09 22:11:17 +0700

    realtime chat

--- components/Common/chatbox/socketUtil.ts
+++ components/Common/chatbox/socketUtil.ts
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io('http://127.0.0.1:6001', { transports : ['websocket'] }); 
+const socket = io('http://127.0.0.1:6001', { transports: ['websocket', 'polling', 'flashsocket']}); 
 
 export default socket;
