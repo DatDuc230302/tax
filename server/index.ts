809dea036f134e4eb244e216e215402a9b1bc163
haotran0103 <tranquochao0102@gmail.com>
2024-01-12 00:20:01 +0700

    deploy

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,3 @@
 export const serverImages: string = 'http://localhost:5000';
-export const serverBackend: string = 'http://localhost:8000';
+export const serverBackend: string = 'http://localhost:5555';
 // export const serverBackend: string = 'http://128.199.226.117:9999';
