b4e345a1c7cdf2cc02414d96cd6585a5e5257e1a
Dat Duc <DatDuc2303@gmail.com>
2024-01-19 14:12:34 +0700

    d

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
             <HotPosts />
             <News />
             <RecentPosts />
-            <Videos />
+            {/* <Videos /> */}
         </div>
     );
 }
